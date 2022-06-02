00caab47d66f418b9c0eeb1cc5f1e026098dd6b3
zhoujiaqi <798911956@qq.com>
2022-06-02 14:06:47 +0800

    POWER first commit

---
+++ nets/darknet2.py
@@ -0,0 +1,141 @@
+import math
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+
+
+#---------------------------------------------------------------------#
+#   残差结构
+#   利用一个1x1卷积下降通道数，然后利用一个3x3卷积提取特征并且上升通道数
+#   最后接上一个残差边
+#---------------------------------------------------------------------#
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channals, **kwargs):
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channals,**kwargs)
+        self.bn = nn.BatchNorm2d(out_channals)
+        self.relu = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+class BasicBlock(nn.Module):
+    def __init__(self, inplanes, planes):
+        super(BasicBlock, self).__init__()
+        self.Ba = nn.Sequential(
+            BasicConv2d(inplanes, planes[0], kernel_size=1, stride=1, padding=0, bias=False),
+            BasicConv2d(planes[0], planes[1], kernel_size=3, stride=1, padding=1, bias=False)
+        )
+    def forward(self, x):
+        residual = x
+        out = self.Ba(x)
+        out += residual
+        return out
+
+
+class DarkNet(nn.Module):
+    def __init__(self, layers):
+        super(DarkNet, self).__init__()
+        self.inplanes = 32
+        # 416,416,3 -> 416,416,32
+        self.layer0 = BasicConv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        # 416,416,32 -> 208,208,64
+        self.layer1 = self._make_layer([32, 64], layers[0])
+        # 208,208,64 -> 104,104,128
+        self.layer2 = self._make_layer([64, 128], layers[1])
+        # 104,104,128 -> 52,52,256
+        self.layer3 = self._make_layer([128, 256], layers[2])
+        # 52,52,256 -> 26,26,512
+        self.layer4 = self._make_layer1([256, 512])
+        # 26,26,512 -> 13,13,1024
+        self.layer5 = self._make_layer2([512, 1024])
+
+        self.layers_out_filters = [64, 128, 256, 512, 1024]
+
+        # 进行权值初始化
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    #---------------------------------------------------------------------#
+    #   在每一个layer里面，首先利用一个步长为2的3x3卷积进行下采样
+    #   然后进行残差结构的堆叠
+    #---------------------------------------------------------------------#
+    def _make_layer(self, planes, blocks):
+        layers = []
+        # 下采样，步长为2，卷积核大小为3
+        layers.append(("ds_conv", nn.Conv2d(self.inplanes, planes[1], kernel_size=3,
+                                stride=2, padding=1, bias=False)))
+        layers.append(("ds_bn", nn.BatchNorm2d(planes[1])))
+        layers.append(("ds_relu", nn.LeakyReLU(0.1)))
+        # 加入残差结构
+        self.inplanes = planes[1]
+        for i in range(0, blocks):
+            layers.append(("residual_{}".format(i), BasicBlock(self.inplanes, planes)))
+        return nn.Sequential(OrderedDict(layers))
+
+    def _make_layer1(self,planes):
+        out = nn.Sequential(
+            BasicConv2d(self.inplanes, planes[1], kernel_size=3, stride=2, padding=1, bias=False),
+            RU_2(planes),
+            BasicBlock(planes[1],planes),
+            RU_2(planes),
+            BasicBlock(planes[1],planes)
+        )
+        self.inplanes = planes[1]
+        return out
+    def _make_layer2(self,planes):
+        out = nn.Sequential(
+            BasicConv2d(self.inplanes, planes[1], kernel_size=3, stride=2, padding=1, bias=False),
+            RU_2(planes),
+            BasicBlock(planes[1], planes),
+        )
+        return out
+
+    def forward(self, x):
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        out3 = self.layer3(x)
+        out4 = self.layer4(out3)
+        out5 = self.layer5(out4)
+
+        return out3, out4, out5
+
+class RU_2(nn.Module):
+    def __init__(self,planes):
+        super(RU_2, self).__init__()
+        self.DBL1 = BasicConv2d(planes[1], planes[0], kernel_size=1, stride=1, padding=0, bias=False)
+        self.DBL2 = BasicConv2d(planes[0], planes[1], kernel_size=3, stride=1, padding=1, bias=False)
+        self.DBL3 = BasicConv2d(planes[1], planes[0], kernel_size=1, stride=1, padding=0, bias=False)
+        self.DBL4 = BasicConv2d(planes[0], planes[1], kernel_size=3, stride=1, padding=1, bias=False)
+
+    def forward(self,x):
+        residual = x
+        out1 = self.DBL1(x)
+        out2 = self.DBL2(out1)
+        out3 = residual + out2
+        residual = out3
+        out4 = self.DBL3(out3)
+        out5 = out1 + out4
+        out6 = self.DBL4(out5)
+        out = residual + out6
+        return out
+
+def darknet53(pretrained, **kwargs):
+    model = DarkNet([2, 3, 7])
+    if pretrained:
+        if isinstance(pretrained, str):
+            model.load_state_dict(torch.load(pretrained))
+        else:
+            raise Exception("darknet request a pretrained path. got [{}]".format(pretrained))
+    return model
